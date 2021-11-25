6aa4ad26c2e7d7ebb3b045e750a503e21601313b
Khan <oguzhanercelik@eskisehir.edu.tr>
2021-11-25 20:03:09 +0300

    home slider fixed and footer reduced

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import "./Slider.css"
 import sample1 from "../../images/sample1.jpg";
 
 import SwiperCore, {
     Navigation,
     Pagination,
-    Autoplay
+    Autoplay,
+    Mousewheel
 } from "swiper/core";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+SwiperCore.use([Navigation, Pagination, Autoplay, Mousewheel]);
 
 export default function Slider() {
 
@@ -23,25 +22,29 @@ export default function Slider() {
 
     return (
         <Swiper
-            cssMode={true}
+            // cssMode={true}
             navigation={true}
-            loop={true}
             autoplay={{
-                "delay": 3000,
+                "delay": 2000,
                 "disableOnInteraction": false
             }}
             pagination={{
                 "type": "progressbar"
             }}
-            style={{ marginTop: "1px" }}
-        >
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s0'></img></SwiperSlide>
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s1'></img></SwiperSlide>
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s2'></img></SwiperSlide>
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s3'></img></SwiperSlide>
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s4'></img></SwiperSlide>
-            <SwiperSlide><img style={{ width: '100%', height: "auto" }} src={sample1} alt='s5'></img></SwiperSlide>
+            slidesPerView={2}
+            centeredSlides={true}
+            spaceBetween={50}
+            mousewheel={false}
 
+            id="home-slider"
+            style={{ marginTop: "2px" }}
+        >
+            <SwiperSlide><img src={"https://picsum.photos/id/5/1024/768"} alt='s0'></img></SwiperSlide>
+            <SwiperSlide><img src={"https://picsum.photos/id/4/1024/768"} alt='s1'></img></SwiperSlide>
+            <SwiperSlide><img src={"https://picsum.photos/id/3/1024/768"} alt='s2'></img></SwiperSlide>
+            <SwiperSlide><img src={"https://picsum.photos/id/2/1024/768"} alt='s3'></img></SwiperSlide>
+            <SwiperSlide><img src={"https://picsum.photos/id/1/1024/768"} alt='s4'></img></SwiperSlide>
+            <SwiperSlide><img src={"https://picsum.photos/id/6/1024/768"} alt='s5'></img></SwiperSlide>
         </Swiper>
 
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,42 +4,32 @@ import Slider from "../components/Slider/Slider";
 import { About } from "../components/About";
 import { Vision } from "../components/Vision";
 import { Team } from "../components/Team";
-import bg2 from "../images/bg2.jpg";
-import bg from "../images/bg.jpg";
 import { ScrollAnimation } from "../components/Animations/ScrollAnimation";
 import { GetPath } from "../components/Animations/ScrollToTop";
 export default function Home() {
-  const [isCancelled, setIsCancelled] = useState(false);
 
-  useEffect(() => {
-    ScrollAnimation();
-  },[GetPath()]);
+    useEffect(() => {
+        ScrollAnimation();
+    }, [GetPath()]);
 
-  return (
-    <>
-      <div className="container-fluid p-0 m-0 position-relative user-select-none ">
-        {/*<div className="overflow-hidden">
-  //    <img className="bg-2" src={bg2} alt='bg-2'></img>
-  //    <img className="bg-1" src={bg} alt='bg-1'></img>
-    </div>*/}
-        <div>
-          <div className="m-0 p-0 ">
-            <Slider></Slider>
-          </div>
+    return (
+        <>
+            <div className="container-fluid p-0 m-0 position-relative user-select-none bg-light-custom">
+                <div className="m-0 p-0 ">
+                    <Slider />
+                </div>
 
-          <div className="m-0 p-0 ">
-            <About></About>
-          </div>
-          <div className="mt-5 pt-5">
-            <Team></Team>
-          </div>
-          <div className="mt-5 pt-5 pb-5">
-            <Vision></Vision>
-          </div>
-          
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+                <div className="m-0 p-0 ">
+                    <About />
+                </div>
+                <div className="mt-5 pt-5">
+                    <Team />
+                </div>
+                <div className="mt-5 pt-5 pb-5">
+                    <Vision />
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 };
