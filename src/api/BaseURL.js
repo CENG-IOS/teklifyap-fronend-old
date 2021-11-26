2b87676f08883d3ae435bf90a499c157e5a7a82a
Khan <oguzhanercelik@eskisehir.edu.tr>
2021-11-26 22:05:44 +0300

    fixed

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:8080/"
+const url = "https://teklifyap-backend.herokuapp.com/"
 export default url;
 //https://teklifyap-backend.herokuapp.com/
 //http://localhost:8080/
